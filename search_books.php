315a7ea1cb996369e4001573bc9c3900047e1b01
dwcs-code-2425 <dwcs.code.2425@gmail.com>
2024-10-29 17:58:02 +0100

    ejemplo searchbook

---
+++ search_books.php
@@ -0,0 +1,74 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Búsqueda</title>
+</head>
+
+<body>
+    <h1>Búsqueda de libros</h1>
+
+    <form method="GET">
+        <label for="busqueda">Introduzca los términos de búsqueda: </label>
+        <input type="search" name="busqueda" id="busqueda" required>
+        <button type="submit">Buscar</button>
+    </form>
+</body>
+
+</html>
+<?php
+if (isset($_GET["busqueda"])) {
+    $terminos_busqueda = $_GET["busqueda"];
+    if (trim($terminos_busqueda) !== "") {
+
+        require_once "connection.php";
+
+        try {
+            $con = getConnection();
+
+            //En la bd bookdb no importan mayúsculas/minúsculas porque está usando collation caseinsensitive, pero no está demás que nuestro código no dependa de la collation de la base de datos
+            $stmt = $con->prepare("select title from books where UPPER(title) like ? ");
+            $stmt->bindValue(1, "%" . strtoupper($terminos_busqueda) . "%");
+
+            //Antes de ejecutar: 
+            // echo "<pre>";
+            // $stmt->debugDumpParams();
+            // echo "</pre>";
+
+            $stmt->execute();
+
+            //Después de ejecutar
+            // echo "<pre>";
+            // $stmt->debugDumpParams();
+            // echo "</pre>";
+
+            $array = $stmt->fetchAll(PDO::FETCH_NUM);
+            if (($array !== false)) {
+                if (!empty($array)) {
+
+                    echo "<ol>";
+                    foreach ($array as $fila_array) {
+                        // un único valor: el title
+                        echo "<li> $fila_array[0] </li>";
+                    }
+                    echo "</ol>";
+                } else {
+                    echo "<p>No se han encontrado resultados</p>";
+                }
+            }
+        } catch (Exception $e) {
+            echo "<p>Ha ocurrido una excepción: " . $e->getMessage() . "</p>";
+        }
+        //Cerramos los recursos
+        $con = null;
+        $stmt = null;
+
+        
+    } else {
+        echo "<p> Introduzca una cadena no vacía </p>";
+    }
+}
+
+?>

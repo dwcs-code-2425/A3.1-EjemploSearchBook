7662bd1b0e8b862096a3ea812a29cf55982413db
cd-dist-2324 <cd.dist.2324@gmail.com>
2025-11-07 11:37:25 +0100

    ++ pasos

--- search_books.php
+++ search_books.php
@@ -26,24 +26,32 @@ if (isset($_GET["busqueda"])) {
         require_once "connection.php";
 
         try {
+            // (1) Crear la conexión
             $con = getConnection();
 
+            // (2) Preparar la consulta
             //En la bd bookdb no importan mayúsculas/minúsculas porque está usando collation caseinsensitive, pero no está demás que nuestro código no dependa de la collation de la base de datos
             $stmt = $con->prepare("select title from books where UPPER(title) like ? ");
+            
+            // (3) Sustituir de los parámetros
             $stmt->bindValue(1, "%" . strtoupper($terminos_busqueda) . "%");
 
             //Antes de ejecutar: 
-            // echo "<pre>";
-            // $stmt->debugDumpParams();
-            // echo "</pre>";
+            echo "<p style='color:blue;'> Información de <code>debugDumpParams</code> <span style='color:red'> antes </span> de llamar a  <code>execute()</code>:</p>";
+            echo "<pre>";
+            $stmt->debugDumpParams();
+            echo "</pre>";
 
+            // (4) Ejecutar la consulta
             $stmt->execute();
 
             //Después de ejecutar
-            // echo "<pre>";
-            // $stmt->debugDumpParams();
-            // echo "</pre>";
+            echo "<p style='color:blue;'> Información de <code>debugDumpParams</code> <span style='color:red'> después </span> de llamar a  <code>execute()</code>:</p>";
+            echo "<pre>";
+            $stmt->debugDumpParams();
+            echo "</pre>";
 
+            // (5) Recuperar los resultados
             $array = $stmt->fetchAll(PDO::FETCH_NUM);
             if (($array !== false)) {
                 if (!empty($array)) {
@@ -58,12 +66,16 @@ if (isset($_GET["busqueda"])) {
                     echo "<p>No se han encontrado resultados</p>";
                 }
             }
+
+            //(6) Capturar excepciones
         } catch (Exception $e) {
             echo "<p>Ha ocurrido una excepción: " . $e->getMessage() . "</p>";
+        } finally {
+            //(7) 
+            // Liberar los recursos
+            $con = null;
+            $stmt = null;
         }
-        //Cerramos los recursos
-        $con = null;
-        $stmt = null;
 
         
     } else {
